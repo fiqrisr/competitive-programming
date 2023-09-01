244d067ab1fc904228b51400f4112bd8247bcdc6
Fiqri Syah Redha <wibulo27@gmail.com>
2023-09-01 23:59:11 +0700

    feat(hackerrank): add mini max sum solution

---
+++ hackerrank/problems/mini-max-sum.cpp
@@ -0,0 +1,27 @@
+/* URL: https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum/problem */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void miniMaxSum(vector<int> arr)
+{
+    long min = arr[0];
+    long max = arr[0];
+    long sum = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    long minSum = sum - max;
+    long maxSum = sum - min;
+
+    cout << minSum << " " << maxSum << endl;
+}
