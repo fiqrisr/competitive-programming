aa76d367d932e11a139b68d8931ce096dd1d1373
Fiqri Syah Redha <wibulo27@gmail.com>
2023-09-01 23:48:18 +0700

    feat(hackerrank): add plus minus solution

---
+++ hackerrank/problems/plus-minus.cpp
@@ -0,0 +1,27 @@
+/* URL: https://www.hackerrank.com/challenges/one-month-preparation-kit-plus-minus/problem */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void plusMinus(vector<int> arr)
+{
+    vector<int> result(3, 0);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > 0)
+            result[0] += 1;
+        if (arr[i] < 0)
+            result[1] += 1;
+        if (arr[i] == 0)
+            result[2] += 1;
+    }
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        float ratio = float(result[i]) / arr.size();
+
+        cout << ratio << setprecision(6) << fixed << endl;
+    }
+}
