1d5ec749179184814f1d13e35f3668185e76abc3
Fiqri Syah Redha <wibulo27@gmail.com>
2023-09-21 15:52:48 +0700

    feat(hackerrank): add flipping bits solution

---
+++ hackerrank/flipping-bits.cpp
@@ -0,0 +1,8 @@
+/* URL: https://www.hackerrank.com/challenges/one-month-preparation-kit-flipping-bits/problem */
+
+using namespace std;
+
+long flippingBits(long n)
+{
+    return n ^ 4294967295;
+}
