7551264e903077c53b59feddf0aff565755b8c1e
Fiqri Syah Redha <wibulo27@gmail.com>
2023-09-21 15:32:07 +0700

    feat(hackerrank): add diagonal difference solution

---
+++ hackerrank/diagonal-difference.cpp
@@ -0,0 +1,20 @@
+/* URL: https://www.hackerrank.com/challenges/one-month-preparation-kit-diagonal-difference/problem */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int diagonalDifference(vector<vector<int>> arr)
+{
+    int diagonalLength = arr[0].size();
+    int ltr = 0;
+    int rtl = 0;
+
+    for (int i = 0; i < diagonalLength; i++)
+    {
+        ltr += arr[i][i];
+        rtl += arr[i][diagonalLength - i - 1];
+    }
+
+    return abs(ltr - rtl);
+}
