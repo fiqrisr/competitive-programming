a8c4f62c40ac53b2bb8fd769e0f9c33d99bb1401
Fiqri Syah Redha <wibulo27@gmail.com>
2024-03-29 10:34:09 +0700

    feat(leetcode): add power of two solution

---
+++ leetcode/power-of-two.cpp
@@ -0,0 +1,13 @@
+/* URL: https://leetcode.com/problems/power-of-two */
+
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n == 0)
+            return false;
+
+        return (ceil(log2(n)) == floor(log2(n)));
+    }
+};
