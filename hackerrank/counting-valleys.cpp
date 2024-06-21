b9bcf44770f294ea0cbc16b9428f1933d45ddd28
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-21 21:26:31 +0700

    feat(hackerrank): add counting valleys solution

---
+++ hackerrank/counting-valleys.cpp
@@ -0,0 +1,29 @@
+/* URL: https://leetcode.com/problems/counting-valleys */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int countingValleys(int steps, string path)
+{
+    int count = 0;
+    int current = 0;
+
+    for (int i = 0; i < path.size(); i++)
+    {
+        int temp = current;
+
+        if (path[i] == 'U')
+            current += 1;
+        else
+            current -= 1;
+
+        if (temp < 0 && current >= 0)
+            count++;
+    }
+
+    return count;
+}
