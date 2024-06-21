ee89033de8b19c92f6e6647d3fd972a12167965c
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-21 20:28:53 +0700

    feat(hackerrank): add permutating two arrays solution

---
+++ hackerrank/permutating-two-arrays.cpp
@@ -0,0 +1,21 @@
+/* URL:
+ * https://www.hackerrank.com/challenges/one-month-preparation-kit-two-arrays/problem
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string twoArrays(int k, vector<int> A, vector<int> B)
+{
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<int>());
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (A[i] + B[i] < k)
+            return "NO";
+    }
+
+    return "YES";
+}
