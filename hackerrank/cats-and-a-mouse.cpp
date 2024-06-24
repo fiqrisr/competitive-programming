eeb672fb23296076fe9ae8a3436ec6c91fc28139
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-25 00:00:31 +0700

    feat(hackerrank): add cats and a mouse solution

---
+++ hackerrank/cats-and-a-mouse.cpp
@@ -0,0 +1,21 @@
+
+/* URL: https://www.hackerrank.com/challenges/one-month-preparation-kit-countingsort1/problem */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+string catAndMouse(int x, int y, int z)
+{
+    int catADistance = abs(z - x);
+    int catBDistance = abs(z - y);
+
+    if (catADistance > catBDistance)
+        return "Cat B";
+    else if (catBDistance > catADistance)
+        return "Cat B";
+    else
+        return "Mouse C";
+}
