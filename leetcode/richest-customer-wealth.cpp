3140cdc9455c0e87b2f7c709b67ad4656961647c
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-25 05:52:47 +0700

    feat(leetcode): add richest customer wealth solution

---
+++ leetcode/richest-customer-wealth.cpp
@@ -0,0 +1,29 @@
+/* URL: https://leetcode.com/problems/richest-customer-wealth */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumWealth(vector<vector<int>> &accounts)
+    {
+        int res = 0;
+
+        for (int i = 0; i < accounts.size(); i++)
+        {
+            int sum = 0;
+
+            for (int j = 0; j < accounts[i].size(); j++)
+            {
+                sum += accounts[i][j];
+            }
+
+            if (sum > res)
+                res = sum;
+        }
+
+        return res;
+    }
+};
