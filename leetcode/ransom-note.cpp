8df6500838043de32201ae51671840b1925efcaa
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-25 17:23:32 +0700

    feat(leetcode): add ransom note solution

---
+++ leetcode/ransom-note.cpp
@@ -0,0 +1,34 @@
+/* URL: https://leetcode.com/problems/ransom-note */
+
+#include <bits/stdc++.h>
+#include <map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        map<char, int> letters;
+        bool res = true;
+
+        for (int i = 0; i < magazine.size(); i++)
+        {
+            if (letters[magazine[i]])
+                letters[magazine[i]] += 1;
+            else
+                letters[magazine[i]] = 1;
+        }
+
+        for (int i = 0; i < ransomNote.size(); i++)
+        {
+            if (letters[ransomNote[i]] == 0)
+                return false;
+
+            letters[ransomNote[i]] -= 1;
+        }
+
+        return res;
+    }
+};
