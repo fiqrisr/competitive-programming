9742dd74701327d233dced369320e98675fb5d35
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-26 06:13:28 +0700

    feat(leetcode): add palindrome number solution

---
+++ leetcode/palindrome-number.cpp
@@ -0,0 +1,25 @@
+/* URL: https://leetcode.com/problems/palindrome-number */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+
+        string num = to_string(x);
+
+        for (int i = 0; i < num.size() / 2; i++)
+        {
+            if (num[i] != num[num.size() - 1 - i])
+                return false;
+        }
+
+        return true;
+    }
+};
