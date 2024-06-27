b1a4ac78a40a38efc13749f97dc49695e14ba8ea
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-27 20:55:22 +0700

    feat(hackerrank): add beautiful days at the movies solution

---
+++ hackerrank/beautiful-days-at-the-movies.cpp
@@ -0,0 +1,31 @@
+/* URL: https://www.hackerrank.com/challenges/beautiful-days-at-the-movies */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int reverseNumber(int n)
+{
+    int rev = 0;
+
+    while (n > 0)
+    {
+        rev = rev * 10 + n % 10;
+        n = n / 10;
+    }
+
+    return rev;
+}
+
+int beautifulDays(int i, int j, int k)
+{
+    int count = 0;
+
+    for (int s = i; s <= j; s++)
+    {
+        if (abs(reverseNumber(s) - s) % k == 0)
+            count++;
+    }
+
+    return count;
+}
