95be0d0524c04852ef2fb4211aee6c2ca7bea151
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-27 18:23:36 +0700

    feat(bigfrontend): add implement Array.prototype.flat solution

---
+++ bigfrontend/3.implement-Array-prototype.flat.ts
@@ -0,0 +1,14 @@
+/* URL: https://bigfrontend.dev/problem/implement-Array-prototype.flat */
+
+function flat(arr: [], depth = 1) {
+  const res = [];
+
+  for (const item of arr) {
+    if (Array.isArray(item) && depth > 0)
+      res.push(...flat(item, depth - 1));
+    else
+      res.push(item);
+  }
+
+  return res;
+}
