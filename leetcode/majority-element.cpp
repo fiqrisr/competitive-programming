54c82fb2096bd03a856f4f55b48d3981c0753bc0
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-28 07:06:10 +0700

    feat(leetcode): add majority element solution

---
+++ leetcode/majority-element.cpp
@@ -0,0 +1,25 @@
+/* URL: https://leetcode.com/problems/majority-element */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int count = 0;
+        int candidate = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (count == 0)
+                candidate = nums[i];
+
+            count += candidate == nums[i] ? 1 : -1;
+        }
+
+        return candidate;
+    }
+};
