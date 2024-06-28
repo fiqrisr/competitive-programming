56eae42b451e98421fca1c780ff4761ed4e86dae
Fiqri Syah Redha <wibulo27@gmail.com>
2024-06-28 07:13:54 +0700

    feat(leetcode): add find first palindromic string in the array solution

---
+++ leetcode/find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,35 @@
+/* URL: https://leetcode.com/problems/find-first-palindromic-string-in-the-array */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] != s[s.size() - 1 - i])
+                return false;
+        }
+
+        return true;
+    }
+
+    string firstPalindrome(vector<string> &words)
+    {
+        string res = "";
+
+        for (int i = 0; i < words.size(); i++)
+        {
+            if (isPalindrome(words[i]))
+            {
+                res = words[i];
+                break;
+            }
+        }
+        return res;
+    }
+};
